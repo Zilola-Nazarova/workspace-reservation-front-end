187a31c8e46dcacee53ec7aeb4ded4519c1422e3
zilola <zilolanazarova1994@gmail.com>
2023-11-30 19:45:23 +0500

    Delete unnecessary greetingSlice and greetingReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
-import greetingsReducer from './greetings/greetingsSlice';
 import workspacesReducer from './workspaces/workspacesSlice';
 import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
-    greetings: greetingsReducer,
     workspaces: workspacesReducer,
     reservations: reservationsReducer,
   },
