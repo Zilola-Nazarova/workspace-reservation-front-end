6ef1b8d072fed0aee85e37b557b3b00caa110038
zilola <zilolanazarova1994@gmail.com>
2023-11-30 19:26:02 +0500

    Add new reducers to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import greetingsReducer from './greetings/greetingsSlice';
+import workspacesReducer from './workspaces/workspacesSlice';
+import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     greetings: greetingsReducer,
+    workspaces: workspacesReducer,
+    reservations: reservationsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
