515d92986fdfc0291245e480bf33fe24bdc7e195
WHB901 <abdulwhbhussain@gmail.com>
2023-12-01 07:26:55 +0500

    Add Signout Logic and Sign out

--- src/routes/SignOutPage.js
+++ src/routes/SignOutPage.js
@@ -1,9 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { clearToken } from '../redux/auth/authSlice';
 import styles from '../styles/SignOutPage.module.css';
 
-const SignOutPage = () => (
-  <div className={styles.page}>
-    <p>This is SignOutPage</p>
-  </div>
-);
+const SignOutPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [goodbyeMessage, setGoodbyeMessage] = useState('');
+
+  useEffect(() => {
+    // Clear user from local storage
+    localStorage.removeItem('user');
+
+    // Set token to null in the Redux store
+    dispatch(clearToken());
+
+    // Show a goodbye message
+    setGoodbyeMessage('Goodbye! Redirecting to the home page in some seconds...');
+
+    // Redirect to the home page after 1 minute
+    const redirectTimeout = setTimeout(() => {
+      navigate('/');
+    }, 30000);
+
+    // Clear the timeout to avoid redirection if the component unmounts
+    return () => clearTimeout(redirectTimeout);
+  }, [dispatch, navigate]);
+
+  return (
+    <div className={styles.page}>
+      <h1>Logout is Complete...</h1>
+      <p>{goodbyeMessage}</p>
+    </div>
+  );
+};
 
 export default SignOutPage;
