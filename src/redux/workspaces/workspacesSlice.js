2aede1fc1b6c4a6f1ca86224ae8cf3cee7bfd2d1
Alfred Peter <alpeter91@gmail.com>
2023-12-02 22:50:18 +0300

    Add code to make a workspace POST request

--- src/redux/workspaces/workspacesSlice.js
+++ src/redux/workspaces/workspacesSlice.js
@@ -12,7 +12,19 @@ export const getWorkspaces = createAsyncThunk(
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
-  },
+  }
+);
+
+export const postWorkspace = createAsyncThunk(
+  'workspaces/postworkspaces',
+  async (newData, thunkAPI) => {
+    try {
+      const response = await axios.post(WORKSPACES_URL, newData);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
 );
 
 const WorkspacesJson = [
@@ -20,31 +32,36 @@ const WorkspacesJson = [
     ID: 1,
     name: 'room#1',
     image: 'https://i.pravatar.cc/300?img=1',
-    description: 'Simple room with 1 seat and 1 talbes, no equipment is provided',
+    description:
+      'Simple room with 1 seat and 1 talbes, no equipment is provided',
   },
   {
     ID: 2,
     name: 'room#2',
     image: 'https://i.pravatar.cc/300?img=2',
-    description: 'Simple room with 2 seats and 1 talbe, no equipment is provided',
+    description:
+      'Simple room with 2 seats and 1 talbe, no equipment is provided',
   },
   {
     ID: 3,
     name: 'room#3',
     image: 'https://i.pravatar.cc/300?img=3',
-    description: 'Large room with 3 seats and 2 talbes, 1 monitor for presentations',
+    description:
+      'Large room with 3 seats and 2 talbes, 1 monitor for presentations',
   },
   {
     ID: 4,
     name: 'room#4',
     image: 'https://i.pravatar.cc/300?img=4',
-    description: 'Large room with 4 seats and 2 talbes, 1 monitor for presentations',
+    description:
+      'Large room with 4 seats and 2 talbes, 1 monitor for presentations',
   },
   {
     ID: 5,
     name: 'room#5',
     image: '',
-    description: 'Large room with 5 seats and 2 talbes, 1 monitor for presentations',
+    description:
+      'Large room with 5 seats and 2 talbes, 1 monitor for presentations',
   },
 ];
 
@@ -57,7 +74,7 @@ const initialState = {
 export const workspacesSlice = createSlice({
   name: 'workspaces',
   initialState,
-  reducers: { },
+  reducers: {},
   extraReducers(builder) {
     builder
       .addCase(getWorkspaces.pending, (state) => {
