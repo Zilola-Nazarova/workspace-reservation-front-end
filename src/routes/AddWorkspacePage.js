ec089179e516a35a4c86fe862a0558b90a688a15
Alfred Peter <alpeter91@gmail.com>
2023-12-02 16:20:04 +0300

    Add new workspace form elements

--- src/routes/AddWorkspacePage.js
+++ src/routes/AddWorkspacePage.js
@@ -1,9 +1,35 @@
 import styles from '../styles/AddWorkspacePage.module.css';
 
-const AddWorkspacePage = () => (
-  <div className={styles.page}>
-    <p>This is AddWorkspacePage</p>
-  </div>
-);
+const AddWorkspacePage = () => {
+  const handleSubmit = () => { };
+  return (
+    <div className={styles.page}>
+      <p>Create New Workspace</p>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="name">
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="description">
+          Description
+          <textarea
+            name="description"
+            id="description"
+            rows="4" // Set the number of visible lines
+            cols="50"
+          />
+        </label>
+        <br />
+        <br />
+        <label htmlFor="image">
+          Image
+          <input type="file" name="image" id="image" />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default AddWorkspacePage;
