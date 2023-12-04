9812137002e1e56170d88a64c3d71face3192d41
Alfred Peter <alpeter91@gmail.com>
2023-12-04 18:10:49 +0300

    Add use-effect to clear form on success

--- src/routes/AddWorkspacePage.js
+++ src/routes/AddWorkspacePage.js
@@ -1,9 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postWorkspace } from '../redux/workspaces/workspacesSlice';
 import styles from '../styles/AddWorkspacePage.module.css';
 
 const AddWorkspacePage = () => {
   const dispatch = useDispatch();
+  const [success, setSuccess] = useState(null);
+  const [fail, setFail] = useState(null);
+  const formRef = useRef(null);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData();
@@ -14,40 +19,56 @@ const AddWorkspacePage = () => {
       data.append('workspace[image]', e.target.image.files[0]);
     }
 
-    console.log(data);
-
     try {
       const resultAction = await dispatch(postWorkspace(data));
-      console.log('Workspace created successfully:', resultAction.payload);
+      if (resultAction.payload.success) {
+        setSuccess(resultAction.payload.success);
+      }
+      if (resultAction.payload.error) {
+        setFail(resultAction.payload.error);
+      }
     } catch (error) {
-      console.error('Failed to create workspace:', error);
+      setFail(error);
     }
   };
 
+  const resetform = () => {
+    formRef.current.reset();
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSuccess(null);
+      setFail(null);
+    }, 3000);
+
+    if (success) {
+      resetform();
+    }
+    return () => clearTimeout(timer);
+  }, [success, fail]);
+
   return (
     <div className={styles.page}>
+      {success && <p>{success}</p>}
+      {fail && <p>{fail}</p>}
       <p>Create New Workspace</p>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form ref={formRef} onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor="name">
           Name
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" required />
         </label>
         <br />
         <br />
         <label htmlFor="description">
           Description
-          <textarea
-            name="description"
-            id="description"
-            rows="4"
-            cols="50"
-          />
+          <textarea name="description" id="description" rows="4" cols="50" required />
         </label>
         <br />
         <br />
         <label htmlFor="image">
           Image
-          <input type="file" name="image" id="image" />
+          <input type="file" name="image" id="image" required />
         </label>
         <button type="submit">Create</button>
       </form>
