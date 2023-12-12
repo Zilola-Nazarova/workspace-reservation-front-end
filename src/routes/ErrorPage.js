c9f590f4afca6859de073b91babb631da74dfa86
Javier <javidx@gmail.com>
2023-12-12 20:10:01 +0200

    Updated the ErrorPage to use ES6 arrow functions

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -1,4 +1,4 @@
-export default function ErrorPage() {
+const ErrorPage = () => {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
@@ -6,3 +6,5 @@ export default function ErrorPage() {
     </div>
   );
 }
+
+export default ErrorPage;
