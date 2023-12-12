5d4a5320f76f8a990f78a47d30e8f90668b4f0f1
zilola <zilolanazarova1994@gmail.com>
2023-12-12 10:24:43 +0500

    Remove the code added fro gh-pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/workspace-reservation-front-end">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
